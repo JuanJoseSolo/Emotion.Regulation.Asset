1dab1c695b87e196807394ad82f20e1748c9b381
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2023-03-05 21:55:32 -0600

    Added four emotional strategies in the BigFiveModel asset

--- EmotionRegulation/BigFiveModel/BigFiveModelAsset.cs
+++ EmotionRegulation/BigFiveModel/BigFiveModelAsset.cs
@@ -3,73 +3,227 @@ using FLS.MembershipFunctions;
 //TODO: Add comments and documentation; add plotting method.
 namespace BigFiveModel
 {
+    /// <summary>
+    /// @public class: this class is used to assign or calculate the dominant personality of an agent.
+    /// </summary>
     public class BigFiveModelAsset
     {
-        IMembershipFunction low;
-        IMembershipFunction middle;
-        IMembershipFunction high;
+        /// Globals
+        float[] personality_level;
+        List<string> traits;
         LinguisticVariable personality;
-        float conscientiousness;
-        float extraversion;
-        float neuroticism;
-        float openness;
-        float agreeableness;
-        float[,] results = new float[5,3];
-        public (float value, string personality) Personality { get => GetDominantPersonality(); }
-
+        LinguisticVariable _strategy;
+        List<LinguisticVariable> personalities;
+        IMembershipFunction high,strong;
+        IMembershipFunction middle, slight;
+        IMembershipFunction low, weak;
         /// <summary>
         /// 
         /// </summary>
-        public BigFiveModelAsset(float conscientiousness, float extraversion, float neuroticism, float openness, float agreeableness)
+        public string Dominant { get => GetDominantPersonality().trait; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public float Value { get => GetDominantPersonality().membership; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="openness"></param>
+        /// <param name="conscientiousness"></param>
+        /// <param name="extraversion"></param>
+        /// <param name="agreeableness"></param>
+        /// <param name="neuroticism"></param>
+        public BigFiveModelAsset(float openness, float conscientiousness,float extraversion, float agreeableness, float neuroticism)
         {
-            this.conscientiousness = conscientiousness;
-            this.extraversion = extraversion;
-            this.neuroticism = neuroticism;
-            this.openness = openness;
-            this.agreeableness = agreeableness;
+            traits = new List<string>() { "Openness","Conscientiousness","Extraversion","Agreeableness","Neuroticism" };
+            personality_level = new float[5] { openness, conscientiousness, extraversion, agreeableness, neuroticism };
             personality = new LinguisticVariable("personality");
+            _strategy = new LinguisticVariable("NONE");
+            personalities = new List<LinguisticVariable>();
             low = personality.MembershipFunctions.AddZShaped("low", 30, 10, 0, 100);
             middle = personality.MembershipFunctions.AddGaussian("middle", 50, 10, 0, 100);
             high = personality.MembershipFunctions.AddSShaped("high", 70, 10, 0, 100);
+            strong = _strategy.MembershipFunctions.AddRectangle("NONE", 1, 1);
+            slight = _strategy.MembershipFunctions.AddRectangle("NONE", 1, 1);
+            weak = _strategy.MembershipFunctions.AddRectangle("NONE", 1, 1);
+            foreach(var trait in traits)
+            {
+                personalities.Add(new LinguisticVariable(trait));
+            }
         }
         /// <summary>
-        /// 
+        ///
         /// </summary>
-        private void Fuzzify() 
+        /// <returns></returns>
+        private (string trait, float membership) GetDominantPersonality()
         {
-            float[] personality_level = new float[5] {conscientiousness, extraversion, neuroticism, openness, agreeableness};
-            results = new float[5,3];
-            for(int i=0; i<5; i++) 
+            // Setting the values for each strategy.
+            float[,] results = new float[traits.Count, 3];
+            for (int i = 0; i < traits.Count; i++)
             {
                 results[i,0] = (float)low.Fuzzify(personality_level[i]);
                 results[i,1] = (float)middle.Fuzzify(personality_level[i]);
                 results[i,2] = (float)high.Fuzzify(personality_level[i]);
             }
-        }
-
-        private (float value, string dominant) GetDominantPersonality()
-        {
-            Fuzzify();
-            double temp_value = 0;
-            (int personality, float value) inf_personality = new (255,255);
-            for(int i=0; i<5; i++)
+            float temp_value = 0;
+            (string trait, float membership) dominant_personality=new();
+            // Finding the grater value for all strategies.
+            for (int i = 0; i < traits.Count; i++)
             {
-                if (temp_value < results[i, 2])
+                if (temp_value < results[i,2])
                 {
-                    temp_value = results[i, 2];
-                    inf_personality.personality = i;
+                    temp_value = results[i,2];
+                    float value = (float)Math.Round(temp_value * 100, 2);
+                    dominant_personality = new (traits[i], value);
                 }
             }
-            float value = (float)Math.Round(temp_value * 100,2);
-            (float value, string personality) dominant_personality = new (value,"None");
-            if(inf_personality.personality == 0) { dominant_personality.personality = "Conscientiousness"; }
-            else if(inf_personality.personality == 1) { dominant_personality.personality = "Extraversion"; }
-            else if(inf_personality.personality == 2) { dominant_personality.personality = "Neuroticism"; }
-            else if(inf_personality.personality == 3) { dominant_personality.personality = "Openness"; }
-            else if(inf_personality.personality == 4) { dominant_personality.personality = "Agreeableness"; }
-            else { dominant_personality.personality = "None"; }
-
             return dominant_personality;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="strategy"></param>
+        private void GenerateLinguisticValues(string strategy)
+        {
+            _strategy = new LinguisticVariable(strategy);
+            weak = _strategy.MembershipFunctions.AddZShaped("weak", 3, 1, 0, 10);
+            slight = _strategy.MembershipFunctions.AddGaussian("slight", 5, 1, 0, 10);
+            strong = _strategy.MembershipFunctions.AddSShaped("strong", 7, 1, 0, 10);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private string GetPertenency(IFuzzyEngine fuzzyEngine)
+        {
+            var defuzzy_result = fuzzyEngine.Defuzzify(new
+            {
+                Openness = (double)personality_level[0],
+                Conscientiousness = (double)personality_level[1],
+                Extraversion = (double)personality_level[2],
+                Agreeableness = (double)personality_level[3],
+                Neuroticism = (double)personality_level[4],
+            });
+
+            Dictionary<string, double> pre_result = new Dictionary<string, double>()
+            {
+                { "Weak", weak.Fuzzify(defuzzy_result) },
+                { "Slight", slight.Fuzzify(defuzzy_result)},
+                { "Strong", strong.Fuzzify(defuzzy_result)}
+            };
+            return pre_result.Aggregate((force, numerical) => force.Value > numerical.Value ? force : numerical).Key;;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string SituationSelection()
+        {
+            // rule1 -> IF Conscientiousness is HIGH and Neuroticism is HIGH then SS is Strong applied.
+            // rule2 -> IF Conscientiousness is MIDDLE and Neuroticism is MIDDLE then SS is Slight applied.
+            // rule3 -> IF Conscientiousness is LOW and Neuroticism is LOW then SS is Weak applied.
+            // rule4 -> IF Extraversion is HIGH and Openness is HIGH and Agreeableness is HIGH then SS is Weak applied.
+            // rule5 -> IF Extraversion is MIDDLE and Openness is MIDDLE and Agreeableness is MIDDLE then SS is Slight applied.
+            // rule6 -> IF Extraversion is LOW and Openness is LOW and Agreeableness is LOW then SS is Strong applies.
+
+            // Openness          -> personalities[0]
+            // Conscientiousness -> personalities[1]
+            // Extraversion      -> personalities[2]
+            // Agreeableness     -> personalities[3]
+            // Neuroticism       -> personalities[4]
+
+            GenerateLinguisticValues("Situation.Selection");
+            var fuzzyEngine = new FuzzyEngineFactory().Default();
+            var rule1 = fuzzyEngine.Rules.If(personalities[1].Is(high).And(personalities[4].Is(high))).Then(_strategy.Is(strong));
+            var rule2 = fuzzyEngine.Rules.If(personalities[1].Is(middle).And(personalities[4].Is(middle))).Then(_strategy.Is(slight));
+            var rule3 = fuzzyEngine.Rules.If(personalities[1].Is(low).And(personalities[4].Is(low))).Then(_strategy.Is(weak));
+            var rule4 = fuzzyEngine.Rules.If(personalities[2].Is(high).And(personalities[0].Is(high)).And(personalities[3].Is(high))).Then(_strategy.Is(weak));
+            var rule5 = fuzzyEngine.Rules.If(personalities[2].Is(middle).And(personalities[0].Is(middle)).And(personalities[3].Is(middle))).Then(_strategy.Is(slight));
+            var rule6 = fuzzyEngine.Rules.If(personalities[2].Is(low).And(personalities[0].Is(low)).And(personalities[3].Is(low))).Then(_strategy.Is(strong));
+            fuzzyEngine.Rules.Add(rule1,rule2,rule3,rule4,rule5,rule6);
+            
+            return "Situation.Selection -> " + GetPertenency(fuzzyEngine);
+        }
+        public string SituationModification()
+        {
+            // rule1 -> IF Neuroticism is HIGH and Agreeableness is HIGH then SM is Weak applied.
+            // rule2 -> IF Neuroticism is MIDDLE and Agreeableness is MIDDLE then SM is Slight applied.
+            // rule3 -> IF Neuroticism is LOW and Agreeableness is LOW then SM is Strong applies.
+            // rule4 -> IF Conscientiousness is HIGH and Extraversion is HIGH and Openness is HIGH then SM is Strong applied.
+            // rule5 -> IF Conscientiousness is MIDDLE and Extraversion is MIDDLE Openness is MIDDLE then SM is Slight applied.
+            // rule6 -> IF Conscientiousness is LOW and Extraversion is LOW and Openness is LOW then SM is Weak applied.
+
+            // Openness          -> personalities[0]
+            // Conscientiousness -> personalities[1]
+            // Extraversion      -> personalities[2]
+            // Agreeableness     -> personalities[3]
+            // Neuroticism       -> personalities[4]
+
+            GenerateLinguisticValues("Situation.Modification");
+            var fuzzyEngine = new FuzzyEngineFactory().Default();
+            var rule1 = fuzzyEngine.Rules.If(personalities[4].Is(high).And(personalities[3].Is(high))).Then(_strategy.Is(weak));
+            var rule2 = fuzzyEngine.Rules.If(personalities[4].Is(middle).And(personalities[3].Is(middle))).Then(_strategy.Is(slight));
+            var rule3 = fuzzyEngine.Rules.If(personalities[4].Is(low).And(personalities[3].Is(low))).Then(_strategy.Is(strong));
+            var rule4 = fuzzyEngine.Rules.If(personalities[1].Is(high).And(personalities[2].Is(high)).And(personalities[0].Is(high))).Then(_strategy.Is(strong));
+            var rule5 = fuzzyEngine.Rules.If(personalities[1].Is(middle).And(personalities[2].Is(middle)).And(personalities[0].Is(middle))).Then(_strategy.Is(slight));
+            var rule6 = fuzzyEngine.Rules.If(personalities[1].Is(low).And(personalities[2].Is(low)).And(personalities[0].Is(low))).Then(_strategy.Is(slight));
+            fuzzyEngine.Rules.Add(rule1,rule2,rule3,rule4,rule5,rule6);
+            
+            return "Situation.Modification -> " + GetPertenency(fuzzyEngine);
+        }
+        public string AttentionalDeployment()
+        {
+            // rule1 -> IF Openness is HIGH and Conscientiousness is HIGH and Agreeableness is HIGH and Extraversion is HIGH then AD is Strong applied.
+            // rule2 -> IF Openness is MIDDLE and Conscientiousness is MIDDLE and Agreeableness is MIDDLE and Extraversion is MIDDLE then AD is Slight applied.
+            // rule3 -> IF Openness is LOW and Conscientiousness is LOW and Agreeableness is LoW and Extraversion is LOW then AD is Weak applied.
+            // rule4 -> IF Neuroticism is HIGH then AD is Weak applied.
+            // rule5 -> IF Neuroticism is MIDDLE then AD is Slight applied.
+            // rule6 -> IF Neuroticism is LOW then AD is Strong applied.
+
+            // Openness          -> personalities[0]
+            // Conscientiousness -> personalities[1]
+            // Extraversion      -> personalities[2]
+            // Agreeableness     -> personalities[3]
+            // Neuroticism       -> personalities[4]
+
+            GenerateLinguisticValues("Attentional.Deployment");
+            var fuzzyEngine = new FuzzyEngineFactory().Default();
+            var rule1 = fuzzyEngine.Rules.If(personalities[0].Is(high).And(personalities[1].Is(high)).And(personalities[3].Is(high)).And(personalities[2].Is(high))).Then(_strategy.Is(strong));
+            var rule2 = fuzzyEngine.Rules.If(personalities[0].Is(slight).And(personalities[1].Is(slight)).And(personalities[3].Is(slight)).And(personalities[2].Is(slight))).Then(_strategy.Is(slight));
+            var rule3 = fuzzyEngine.Rules.If(personalities[0].Is(low).And(personalities[1].Is(low)).And(personalities[3].Is(low)).And(personalities[2].Is(low))).Then(_strategy.Is(weak));
+            var rule4 = fuzzyEngine.Rules.If(personalities[4].Is(high)).Then(_strategy.Is(weak));
+            var rule5 = fuzzyEngine.Rules.If(personalities[4].Is(middle)).Then(_strategy.Is(slight));
+            var rule6 = fuzzyEngine.Rules.If(personalities[4].Is(low)).Then(_strategy.Is(strong));
+            fuzzyEngine.Rules.Add(rule1,rule2,rule3,rule4,rule5,rule6);
+            
+            return "Attentional.Deployment -> " + GetPertenency(fuzzyEngine);
+        }
+        public string CongnitiveChange()
+        {
+            // rule1 -> IF Openness is HIGH and Conscientiousness is HIGH and Agreeableness is HIGH and Extraversion is HIGH then AD is Strong applied.
+            // rule2 -> IF Openness is MIDDLE and Conscientiousness is MIDDLE and Agreeableness is MIDDLE and Extraversion is MIDDLE then AD is Slight applied.
+            // rule3 -> IF Openness is LOW and Conscientiousness is LOW and Agreeableness is LoW and Extraversion is LOW then AD is Weak applied.
+            // rule4 -> IF Neuroticism is HIGH then AD is Weak applied.
+            // rule5 -> IF Neuroticism is MIDDLE then AD is Slight applied.
+            // rule6 -> IF Neuroticism is LOW then AD is Strong applied.
+
+            // Openness          -> personalities[0]
+            // Conscientiousness -> personalities[1]
+            // Extraversion      -> personalities[2]
+            // Agreeableness     -> personalities[3]
+            // Neuroticism       -> personalities[4]
+
+            GenerateLinguisticValues("Attentional.Deployment");
+            var fuzzyEngine = new FuzzyEngineFactory().Default();
+            var rule1 = fuzzyEngine.Rules.If(personalities[0].Is(high).And(personalities[1].Is(high)).And(personalities[3].Is(high)).And(personalities[2].Is(high))).Then(_strategy.Is(strong));
+            var rule2 = fuzzyEngine.Rules.If(personalities[0].Is(slight).And(personalities[1].Is(slight)).And(personalities[3].Is(slight)).And(personalities[2].Is(slight))).Then(_strategy.Is(slight));
+            var rule3 = fuzzyEngine.Rules.If(personalities[0].Is(low).And(personalities[1].Is(low)).And(personalities[3].Is(low)).And(personalities[2].Is(low))).Then(_strategy.Is(weak));
+            var rule4 = fuzzyEngine.Rules.If(personalities[4].Is(high)).Then(_strategy.Is(weak));
+            var rule5 = fuzzyEngine.Rules.If(personalities[4].Is(middle)).Then(_strategy.Is(slight));
+            var rule6 = fuzzyEngine.Rules.If(personalities[4].Is(low)).Then(_strategy.Is(strong));
+            fuzzyEngine.Rules.Add(rule1,rule2,rule3,rule4,rule5,rule6);
+            
+            return "Attentional.Deployment -> " + GetPertenency(fuzzyEngine);
         }
     }
 }
---
+++ EmotionRegulation/Test.EmotionRegulationAsset/Test_TheBigFiveModel.cs
@@ -0,0 +1,127 @@
+using EmotionRegulation;
+using BigFiveModel;
+using System.Diagnostics;
+
+namespace Test.EmotionRegulationAsset
+{
+    [TestClass]
+    public class Test_TheBigFiveModel
+    {
+        [TestMethod]
+        public void Test_SituationSelection()
+        {
+            // Arrange
+            BigFiveModelAsset bfm_openness = new BigFiveModelAsset(openness: 99, conscientiousness: 10, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_conscientiousness = new BigFiveModelAsset(openness: 10, conscientiousness: 99, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_extraversion = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 99, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_agreeableness = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 99, neuroticism: 20);
+            BigFiveModelAsset bfm_neuroticism = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 20, neuroticism: 99);
+            string TestingSituationSelection(BigFiveModelAsset personality) { return personality.SituationSelection(); }
+            // Act
+            string openness = TestingSituationSelection(bfm_openness);
+            string conscientiousness = TestingSituationSelection(bfm_conscientiousness);
+            string extraversion = TestingSituationSelection(bfm_extraversion);
+            string agreeableness = TestingSituationSelection(bfm_agreeableness);
+            string neuroticism = TestingSituationSelection(bfm_neuroticism);
+            // Print outputs
+            Debug.Print($"{bfm_openness.Dominant} -> {openness}");
+            Debug.Print($"{bfm_conscientiousness.Dominant} -> {conscientiousness}");
+            Debug.Print($"{bfm_extraversion.Dominant} -> {extraversion}");
+            Debug.Print($"{bfm_agreeableness.Dominant} -> {agreeableness}");
+            Debug.Print($"{bfm_neuroticism.Dominant} -> {neuroticism}");
+            // Assert
+            Assert.IsTrue(openness.Contains("Weak"));
+            Assert.IsTrue(conscientiousness.Contains("Strong"));
+            Assert.IsTrue(extraversion.Contains("Weak"));
+            Assert.IsTrue(agreeableness.Contains("Weak"));
+            Assert.IsTrue(neuroticism.Contains("Strong"));
+        }
+        [TestMethod]
+        public void Test_SituationModification()
+        {
+            // Arrange
+            BigFiveModelAsset bfm_openness = new BigFiveModelAsset(openness: 99, conscientiousness: 10, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_conscientiousness = new BigFiveModelAsset(openness: 10, conscientiousness: 99, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_extraversion = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 99, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_agreeableness = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 99, neuroticism: 20);
+            BigFiveModelAsset bfm_neuroticism = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 20, neuroticism: 99);
+            string TestingSituationModification(BigFiveModelAsset personality){ return personality.SituationModification(); }
+            // Act
+            string openness = TestingSituationModification(bfm_openness);
+            string conscientiousness = TestingSituationModification(bfm_conscientiousness);
+            string extraversion = TestingSituationModification(bfm_extraversion);
+            string agreeableness = TestingSituationModification(bfm_agreeableness);
+            string neuroticism = TestingSituationModification(bfm_neuroticism);
+            // Print outputs
+            Debug.Print($"{bfm_openness.Dominant} -> {openness}");
+            Debug.Print($"{bfm_conscientiousness.Dominant} -> {conscientiousness}");
+            Debug.Print($"{bfm_extraversion.Dominant} -> {extraversion}");
+            Debug.Print($"{bfm_agreeableness.Dominant} -> {agreeableness}");
+            Debug.Print($"{bfm_neuroticism.Dominant} -> {neuroticism}");
+            // Assert
+            Assert.IsTrue(openness.Contains("Strong"));
+            Assert.IsTrue(conscientiousness.Contains("Strong"));
+            Assert.IsTrue(extraversion.Contains("Strong"));
+            Assert.IsTrue(agreeableness.Contains("Weak") || agreeableness.Contains("Slight"));
+            Assert.IsTrue(neuroticism.Contains("Weak")  || neuroticism.Contains("Slight"));
+        }
+        [TestMethod]
+        public void Test_AttentionalDeployment()
+        {
+            // Arrange
+            BigFiveModelAsset bfm_openness = new BigFiveModelAsset(openness: 99, conscientiousness: 10, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_conscientiousness = new BigFiveModelAsset(openness: 10, conscientiousness: 99, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_extraversion = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 99, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_agreeableness = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 99, neuroticism: 20);
+            BigFiveModelAsset bfm_neuroticism = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 20, neuroticism: 99);
+            string TestingAttentionalDeployment(BigFiveModelAsset personality) { return personality.AttentionalDeployment(); }
+            // Act
+            string openness = TestingAttentionalDeployment(bfm_openness);
+            string conscientiousness = TestingAttentionalDeployment(bfm_conscientiousness);
+            string extraversion = TestingAttentionalDeployment(bfm_extraversion);
+            string agreeableness = TestingAttentionalDeployment(bfm_agreeableness);
+            string neuroticism = TestingAttentionalDeployment(bfm_neuroticism);
+            // Print outputs
+            Debug.Print($"{bfm_openness.Dominant} -> {openness}");
+            Debug.Print($"{bfm_conscientiousness.Dominant} -> {conscientiousness}");
+            Debug.Print($"{bfm_extraversion.Dominant} -> {extraversion}");
+            Debug.Print($"{bfm_agreeableness.Dominant} -> {agreeableness}");
+            Debug.Print($"{bfm_neuroticism.Dominant} -> {neuroticism}");
+            // Assert
+            Assert.IsTrue(openness.Contains("Strong"));
+            Assert.IsTrue(extraversion.Contains("Strong"));
+            Assert.IsTrue(agreeableness.Contains("Strong"));
+            Assert.IsTrue(conscientiousness.Contains("Strong"));
+            Assert.IsTrue(neuroticism.Contains("Weak") || neuroticism.Contains("Slight"));
+        }
+        [TestMethod]
+        public void Test_CognitiveChange()
+        {
+            // Arrange
+            BigFiveModelAsset bfm_openness = new BigFiveModelAsset(openness: 99, conscientiousness: 10, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_conscientiousness = new BigFiveModelAsset(openness: 10, conscientiousness: 99, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_extraversion = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 99, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_agreeableness = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 99, neuroticism: 20);
+            BigFiveModelAsset bfm_neuroticism = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 20, neuroticism: 99);
+            string TestingCognitiveChange(BigFiveModelAsset personality) { return personality.CongnitiveChange(); }
+            // Act
+            string openness = TestingCognitiveChange(bfm_openness);
+            string conscientiousness = TestingCognitiveChange(bfm_conscientiousness);
+            string extraversion = TestingCognitiveChange(bfm_extraversion);
+            string agreeableness = TestingCognitiveChange(bfm_agreeableness);
+            string neuroticism = TestingCognitiveChange(bfm_neuroticism);
+            // Print outputs
+            Debug.Print($"{bfm_openness.Dominant} -> {openness}");
+            Debug.Print($"{bfm_conscientiousness.Dominant} -> {conscientiousness}");
+            Debug.Print($"{bfm_extraversion.Dominant} -> {extraversion}");
+            Debug.Print($"{bfm_agreeableness.Dominant} -> {agreeableness}");
+            Debug.Print($"{bfm_neuroticism.Dominant} -> {neuroticism}");
+            // Assert
+            Assert.IsTrue(openness.Contains("Strong"));
+            Assert.IsTrue(extraversion.Contains("Strong"));
+            Assert.IsTrue(agreeableness.Contains("Strong"));
+            Assert.IsTrue(conscientiousness.Contains("Strong"));
+            Assert.IsTrue(neuroticism.Contains("Weak") || neuroticism.Contains("Slight"));
+        }
+    }
+}
