c139b7c3c028b82f166185f4c78238a5666b3c1d
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2023-03-05 22:31:16 -0600

    Added Response Modulation into BigFiveModelAsset

--- EmotionRegulation/Test.EmotionRegulationAsset/Test_TheBigFiveModel.cs
+++ EmotionRegulation/Test.EmotionRegulationAsset/Test_TheBigFiveModel.cs
@@ -123,5 +123,34 @@ namespace Test.EmotionRegulationAsset
             Assert.IsTrue(conscientiousness.Contains("Strong"));
             Assert.IsTrue(neuroticism.Contains("Weak") || neuroticism.Contains("Slight"));
         }
+        [TestMethod]
+        public void Test_ResponseModulation()
+        {
+            // Arrange
+            BigFiveModelAsset bfm_openness = new BigFiveModelAsset(openness: 1, conscientiousness: 10, extraversion: 20, agreeableness: 10, neuroticism: 20);
+            BigFiveModelAsset bfm_conscientiousness = new BigFiveModelAsset(openness: 10, conscientiousness: 1, extraversion: 20, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_extraversion = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 1, agreeableness: 30, neuroticism: 20);
+            BigFiveModelAsset bfm_agreeableness = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 9, neuroticism: 20);
+            BigFiveModelAsset bfm_neuroticism = new BigFiveModelAsset(openness: 10, conscientiousness: 20, extraversion: 30, agreeableness: 20, neuroticism: 1 );
+            string TestingCognitiveChange(BigFiveModelAsset personality) { return personality.ResponseModulation(); }
+            // Act
+            string openness = TestingCognitiveChange(bfm_openness);
+            string conscientiousness = TestingCognitiveChange(bfm_conscientiousness);
+            string extraversion = TestingCognitiveChange(bfm_extraversion);
+            string agreeableness = TestingCognitiveChange(bfm_agreeableness);
+            string neuroticism = TestingCognitiveChange(bfm_neuroticism);
+            // Print outputs
+            Debug.Print($"{bfm_openness.Dominant} -> {openness}");
+            Debug.Print($"{bfm_conscientiousness.Dominant} -> {conscientiousness}");
+            Debug.Print($"{bfm_extraversion.Dominant} -> {extraversion}");
+            Debug.Print($"{bfm_agreeableness.Dominant} -> {agreeableness}");
+            Debug.Print($"{bfm_neuroticism.Dominant} -> {neuroticism}");
+            // Assert
+            Assert.IsTrue(openness.Contains("Strong"));
+            Assert.IsTrue(extraversion.Contains("Strong"));
+            Assert.IsTrue(agreeableness.Contains("Strong"));
+            Assert.IsTrue(conscientiousness.Contains("Strong"));
+            Assert.IsTrue(neuroticism.Contains("Strong"));
+        }
     }
 }
