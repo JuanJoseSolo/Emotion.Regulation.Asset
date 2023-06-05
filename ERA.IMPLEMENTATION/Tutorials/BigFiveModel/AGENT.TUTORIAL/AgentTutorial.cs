c883f374500285dcbad18774f91e1d7826b9a7c3
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2023-06-04 20:49:28 -0600

    Added class 'FAtiMAConnection'

--- ERA.IMPLEMENTATION/Tutorials/BigFiveModel/AGENT.TUTORIAL/AgentTutorial.cs
+++ ERA.IMPLEMENTATION/Tutorials/BigFiveModel/AGENT.TUTORIAL/AgentTutorial.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using BigFiveModel;
 
 namespace BigFiveModel.Tutorial
@@ -10,7 +11,10 @@ namespace BigFiveModel.Tutorial
             // Create an Agent using the class Agent.
             // The class 'Agent' inherits from the class 'BigFiveModelAsset, so, you need pass as parameters the value
             // for each trait.
-            AgentAsset myAgent = new AgentAsset(openness:0.0f,conscientiousness:0.0f,extraversion:0.0f,agreeableness:0.0f,neuroticism:0.0f);
+            AgentAsset myAgent = new AgentAsset(openness: 0.0f, conscientiousness: 0.0f, extraversion: 0.0f, agreeableness: 0.0f, neuroticism: 0.0f);
+            myAgent.Name = "Pedro";
+            // Now, we need to link our agent to the FAtiMA character, to do this we need to indicate where is the .json FAtiMA files.
+            myAgent.FAtiMAInfo = new ERA.Utilities.FAtiMAConnection(@"D:\Git\Emotion.Regulation.Asset\Scenarios\");
 
             // The agent can have: a dominant personality; a value of pertenency of its personality; and, specific Emotional Regulation strategies.
             var dominant = myAgent.Dominant;
@@ -22,8 +26,11 @@ namespace BigFiveModel.Tutorial
             strategies.ForEach(strategy => Console.WriteLine(" - " + strategy.ToString()));
             Console.WriteLine("The level of the all strategies are: ");
             allStrategies.ForEach(strategy => Console.WriteLine(" - " + strategy.ToString()));
-            // Now, we need to link our agent to the FAtiMA character:
-            myAgent.GetCharacterFromJSON(@"D:\Git\Emotion.Regulation.Asset\Scenarios\");
+            StartScenario(myAgent);
+        }
+        static void StartScenario(AgentAsset agent)
+        {
+            var a = agent.FAtiMAInfo.FAtiMA_Assets;
         }
     }
 }
---
+++ ERA.IMPLEMENTATION/Utilities/Utilities/FAtiMAConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using IntegratedAuthoringTool;
+using GAIPS.Rage;
+
+namespace ERA.Utilities
+{
+    public class FAtiMAConnection
+    {
+        
+        public IntegratedAuthoringToolAsset FAtiMA_Assets { get => iat; }
+        string pathFiles = string.Empty;
+        IntegratedAuthoringToolAsset iat;
+        public FAtiMAConnection(string pathFiles) 
+        {
+            iat = new IntegratedAuthoringToolAsset();
+            this.pathFiles = pathFiles;
+            this.GetFAtiMAFromJSON();
+        }
+        private void GetFAtiMAFromJSON()
+        {
+            /// To use a GAIPS.Rage library, you need to compaile the FAtiMA-AuthoringTools project. 
+            Directory.SetCurrentDirectory(pathFiles);
+            var files = Directory.GetFiles(pathFiles);
+            string scenario_path = files[0];
+            string storage_path = files[1];
+            var storage = AssetStorage.FromJson(File.ReadAllText(storage_path));
+            iat = IntegratedAuthoringToolAsset.FromJson(File.ReadAllText(scenario_path),storage);
+        }
+    }
+}
